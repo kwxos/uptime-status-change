c4a1a89a61207f5d4b714af8bcbf059d778d5598
静雨·安婵 <102129419+kwxos@users.noreply.github.com>
2023-01-27 16:22:59 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'UPtime监控',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1993681-3ea46c34da0e59eb28558eee1',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '静雨·安蝉’Blog',
+      url: 'https://blog.kwxos.top'
+    },
+  ],
+};
